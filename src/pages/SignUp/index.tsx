365d4be3b2a97debfb2a083c2de2bfc53d6dba8e
bruno-wolf <bruno.m.lobato@gmail.com>
2020-12-02 19:25:58 +0000

    adds SignUp page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
-import { Container } from './styles'
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
+
+
+const logoImg = require('../../assets/logo.png');
 
 const SignUp: React.FC = () => {
-  return <Container />
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Crie a sua conta</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="nome" />
+            <Input name="email" icon="mail" placeholder="email" />
+            <Input name="password" icon="lock" placeholder="palavra-passe" />
+            <Button onPress={() => { }}>Registar</Button>
+
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <BackToSignIn onPress={() => { }}>
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <BackToSignInText>Voltar</BackToSignInText>
+      </BackToSignIn>
+    </>
+  )
 }
 
 export default SignUp;
