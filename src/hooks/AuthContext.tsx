22aa6d03ccb0417cc811d7dd9eba5d6bade59921
bruno-wolf <bruno.m.lobato@gmail.com>
2020-12-03 18:58:59 +0000

    adds route for authenticated user with temp dashboard

--- src/hooks/AuthContext.tsx
+++ src/hooks/AuthContext.tsx
@@ -19,6 +19,7 @@ interface SignInCredentials {
 }
 interface AuthContextData {
   user: object;
+  loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
@@ -27,6 +28,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStorageData(): Promise<void> {
@@ -38,6 +40,8 @@ export const AuthProvider: React.FC = ({ children }) => {
       if (token && user) {
         setData({ token: token[1], user: JSON.parse(user[1]) })
       }
+
+      setLoading(false);
     }
 
     loadStorageData();
@@ -69,7 +73,7 @@ export const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, loading, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
