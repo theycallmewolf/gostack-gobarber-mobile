b08882461ac7bf25123df61c050a18572f793613
bruno-wolf <bruno.m.lobato@gmail.com>
2021-01-18 21:24:13 +0000

    adds react-native-flipper dependency

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -18,6 +18,8 @@ export const Header = styled.View`
   align-items: center;
 `;
 
+Header.displayName = 'DashboardHeader';
+
 export const HeaderTitle = styled.Text`
   color: #f4ede8;
   font-size: 20px;
